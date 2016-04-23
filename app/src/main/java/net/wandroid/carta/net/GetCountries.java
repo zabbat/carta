fe4f000b530463fbf1042cab66aa7c89fbc3153c
Johan Jungbeck <johan.jungbeck@wandroid.net>
2016-04-23 17:46:00 +0200

    Fix url encoding for space
    space should be replaced with %20

--- app/src/main/java/net/wandroid/carta/net/GetCountries.java
+++ app/src/main/java/net/wandroid/carta/net/GetCountries.java
@@ -8,6 +8,8 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
 
 /**
  * Class for a Rest GET for countries
@@ -17,6 +19,9 @@ public class GetCountries {
     public static final String GET = "GET";
     public static final int TIMEOUT_MILLIS = 30 * 1000;
     public static final String HTTPS_RESTCOUNTRIES_EU_REST_V1_NAME = "https://restcountries.eu/rest/v1/name/";
+    public static final String UTF_8 = "UTF-8";
+    public static final String URLENCODE_SPACE = "+";
+    public static final String REST_SPACE = "%20";
 
     /**
      * Returns an array of matching countries
@@ -27,7 +32,8 @@ public class GetCountries {
      * @throws IOException
      */
     public Country[] getCountries(String name) throws IOException {
-        String url = HTTPS_RESTCOUNTRIES_EU_REST_V1_NAME + name;
+        String url = HTTPS_RESTCOUNTRIES_EU_REST_V1_NAME + URLEncoder.encode(name, UTF_8).replace(URLENCODE_SPACE, REST_SPACE);
+
         return getData(url, Country[].class);
     }
 
